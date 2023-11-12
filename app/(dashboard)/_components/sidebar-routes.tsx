78a5a7a175f4491508bf6a3c48ce1706cc8a30f4
Ryan Finnie <monstermash2008@gmail.com>
2023-11-13 00:06:31 +1300

    feat: UI for creating courses

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-import { Compass, Layout } from "lucide-react";
+import { BarChart, Compass, Layout, List } from "lucide-react";
 import SidebarItem from "./sidebar-item";
+import { usePathname } from "next/navigation";
 
 const SidebarRoutes = () => {
+  const pathname = usePathname();
+  const isTeacherPage = pathname?.includes("/teacher");
+
   const guestRoutes = [
     {
       icon: Layout,
@@ -17,7 +21,20 @@ const SidebarRoutes = () => {
     },
   ];
 
-  const routes = guestRoutes;
+  const teacherRoutes = [
+    {
+      icon: List,
+      label: "Courses",
+      href: "/teacher/courses",
+    },
+    {
+      icon: BarChart,
+      label: "Analytics",
+      href: "/teacher/analytics",
+    },
+  ];
+
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
   return (
     <div className="flex flex-col w-full">
       {routes.map((r) => (
